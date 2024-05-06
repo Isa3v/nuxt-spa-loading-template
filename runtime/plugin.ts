c7619ac887212acde224b8ebe3222583ad282130
Isaev Danil <danil.isaev@yahoo.com>
2024-05-06 07:04:14 +0000

    fix: Tempalte compilation without lodash.template | Support default spa-loading-template | prettierrc

--- runtime/plugin.ts
+++ runtime/plugin.ts
@@ -1,4 +1,4 @@
-import { defineNuxtPlugin } from '#app'
+import { defineNuxtPlugin } from '#app';
 
 // This Nuxt plugin performs the following actions:
 // 1. Creates and mounts a component in the DOM to display a loading state.
@@ -8,19 +8,16 @@ export default defineNuxtPlugin((nuxtApp) => {
     // Check if the 'document' object and its 'body' property exist.
     if (document && document.body) {
         // Create a 'div' element to display the loading component.
-        const loaderHtmlComponent = document.createElement('div')
+        const loaderHtmlComponent = document.createElement('div');
         // Insert the HTML code for the loading component, specified in the plugin options.
-        loaderHtmlComponent.innerHTML = `<%= options.html %>`
+        loaderHtmlComponent.innerHTML = `<%= options.html %>`;
         // Insert the loading component at the beginning of the document's body.
-        document.body.insertBefore(
-            loaderHtmlComponent,
-            document.body.firstChild,
-        )
+        document.body.insertBefore(loaderHtmlComponent, document.body.firstChild);
 
         function removeHTMLComponent() {
             if (loaderHtmlComponent) {
                 // Remove the loading component.
-                loaderHtmlComponent.remove()
+                loaderHtmlComponent.remove();
             }
         }
 
@@ -28,14 +25,14 @@ export default defineNuxtPlugin((nuxtApp) => {
         nuxtApp.hook('page:finish', () => {
             setTimeout(() => {
                 // Remove the loading component.
-                removeHTMLComponent()
-            }, Number(`<%= options.delay %>`))
-        })
+                removeHTMLComponent();
+            }, Number(`<%= options.delay %>`));
+        });
 
         // Remove element after fatal error occures
         nuxtApp.hook('app:error', () => {
             // Remove the loading component.
-            removeHTMLComponent()
-        })
+            removeHTMLComponent();
+        });
     }
-})
+});
